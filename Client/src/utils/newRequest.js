39f3bf2d4e4e2f1366721c11b37fba3019d9902b
Prince lohia <princelohia444@gmail.com>
2026-01-14 20:40:21 +0530

    fix : updated baseURL

--- Client/src/utils/newRequest.js
+++ Client/src/utils/newRequest.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const newRequest = axios.create({
   // baseURL: "http://localhost:3000/api/",
-  baseURL: "https://gigflow-api.onrender.com/api/",
+  baseURL: "https://gigflow-platform-lfjc.onrender.com/api/",
   withCredentials: true,
 });
 
